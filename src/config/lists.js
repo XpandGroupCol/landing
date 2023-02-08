6ee2e51d5d04a2d588d16d6e961c6cd1bc9124d2
diego-contreras <diego.contreras@globant.com>
2023-02-08 11:25:53 -0500

    init astro landinga page

---
+++ src/config/lists.js
@@ -0,0 +1,25 @@
+export const ALL_IN_ONE = [
+    {
+        number: 1,
+        title: '¡Controla tus campañas!',
+        description_one: 'Maneja todas tus campañas de marketing en una sola plataforma',
+        description_two: 'Realiza el seguimiento del desempeño que estás teniendo en tu marca'
+    },
+    {
+        number: 2,
+        title: '¡Maneja tu estrategia!',
+        description_one: 'Gestiona tu plan de marketing en una sola plataforma',
+        description_two: 'Recibe recomendaciones para alcanzar más clientes que harán crecer tu negocio'
+    },
+    {
+        number: 3,
+        title: '¡Conoce tus datos!',
+        description_one: 'Analiza el resultado de tu campaña y el comportamiento de tus clientes',
+        description_two: 'Obtén los enfoques que debes tener para guiar a tu negocio al crecimiento y cumplimiento de objetivos'
+    }
+]
+
+
+
+
+
