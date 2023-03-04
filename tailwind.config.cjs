74d5c63e74933ea9266f21688a61b465109f342f
diego-contreras <diego.contreras@globant.com>
2023-03-04 14:02:07 -0500

    add all in one, countdown and women section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#9747FF',
+				secondary: '#2D2D2C'
+			}
+		},
 	},
 	plugins: [],
 }
