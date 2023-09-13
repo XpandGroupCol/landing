088bba892acdb61171a7826c85a99e5a3732f1c1
XpandGroupCol <Developer@xpandgroup.co>
2023-09-13 11:55:45 -0500

    no da

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,14 +3,20 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'image-banner': "url('./public/fondo.banner.svg')"},
 			colors: {
 				primary: '#9747FF',
 				secondary: '#2D2D2C'
 			},
 			fontFamily: {
+				// 'ceraBold': ['Cera Bold', 'sans-serif'],
+				//'ceraLight': ['Cera Light', 'sans-serif'],
 				ceraBold: "'Cera Bold', serif",
 				ceraNormal: "'Cera Normal', serif",
 				helvetica: "'Helvetica', serif",
+				ceraThin:"'Cera Thin',serif",
+				ceraLight:"'Cera Light', serif"
 			},
 			screens: {
 				xs: '450px'
