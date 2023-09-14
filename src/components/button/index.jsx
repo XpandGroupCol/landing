a1cccfcb7971cd4e0cb42318076c321b87c25237
XpandGroupCol <Developer@xpandgroup.co>
2023-09-13 20:52:08 -0500

    despues me da

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,8 +1,12 @@
 const Button = ({text, onClick}) => {
+    const words =text.split(' ');
     return (
         <button onClick={onClick}
-         className='bg-primary font-ceraBold border-none text-thirduppercase  h-[48px] text-[20px] leading-[42px] w-full rounded-xl px-6 flex justify-center items-center whitespace-nowrap'>
-            {text}
+         className='bg-primary font-ceraBold border-none text-third capitalize  h-[46px]
+          text-[20px] leading-[42px] w-full rounded-[30px] px-6 flex justify-center items-center whitespace-nowrap'>
+             <span className="text-black">{words[0]}</span>
+            <span className="text-pink ml-1">{words[1]}</span>
+            
         </button>
     )
 }
