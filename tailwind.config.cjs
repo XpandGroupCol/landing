01ea343469f0204d188f5d85cce928628448eb0a
XpandGroupCol <Developer@xpandgroup.co>
2023-09-14 13:45:04 -0500

    okay

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#FFFFFF',
-				secondary: '#D9D9D9',
-				third: '#E62057',
-			},
-			backgroundImage: {
-				'image-banner': "url('/fondo.banner.svg')"},
-				
-			fontFamily: {
-				ceraBold: "'Cera Bold', serif",
-				ceraNormal: "'Cera Normal', serif",
-				helvetica: "'Helvetica', serif",
-				ceraThin:"'Cera Thin',serif",
-				ceraLight:"'Cera Light', serif'",
-			},
-			screens: {
-				xs: '450px'
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        secondary: "#D9D9D9",
+        third: "#E62057",
+      },
+      backgroundImage: {
+        "image-banner": "url('/fondo.banner.svg')",
+      },
+
+      fontFamily: {
+        ceraBold: "'Cera Bold', serif",
+        ceraNormal: "'Cera Normal', serif",
+        helvetica: "'Helvetica', serif",
+        ceraThin: "'Cera Thin',serif",
+        ceraLight: "'Cera Light', serif'",
+      },
+      lineHeight: {
+        70: "70px",
+        33: "33px",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
+  },
+  plugins: [],
+};
