78731f2220e27d96fbc9d52a6dbb72bdbaa37a88
XpandGroupCol <Developer@xpandgroup.co>
2023-09-14 07:41:37 -0500

    okay

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -10,14 +10,19 @@
 
 export default Button
 */
-const Button = ({text, onClick}) => {
-    //const words =text.split(' ');
+const Button = ({ text, onClick }) => {
+    const words = text.split(' ');
+
     return (
-        <button onClick={onClick}
-         className='bg-primary font-ceraBold border-none text-third   h-[46px]
-          text-[20px] leading-[42px] w-full rounded-[30px] px-6 flex justify-center items-center whitespace-nowrap'>
-            
-            {text}
+        <button onClick={onClick} className='bg-white font-ceraBold border-none h-[46px] text-[20px] leading-[42px] w-full rounded-[30px] px-6 flex justify-center items-center whitespace-nowrap'>
+            {words.length === 2 ? (
+                <>
+                    <span className="text-third">{words[0]}</span> {/* Este será el color rosado */}
+                    <span className="text-black ml-1">{words[1]}</span> {/* Este será el color negro */}
+                </>
+            ) : (
+                text
+            )}
         </button>
     )
 }
