f2569a216f0be8d5b247054aa80a5076167ff987
XpandGroupCol <Developer@xpandgroup.co>
2023-09-15 17:24:49 -0500

    funciona el texto de color

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -32,7 +32,7 @@ const Button = ({
   return (
     <button
       onClick={onClick}
-      className={`${textcolor}${bgcolor} font-ceraBold border-none h-[46px] text-[24px] leading-[42px] w-full rounded-[30px] px-6 flex justify-center items-center whitespace-nowrap`}
+      className={`${textcolor} ${bgcolor} font-ceraBold border-none h-[46px] text-[24px] leading-[42px] w-full rounded-[30px] px-6 flex justify-center items-center whitespace-nowrap`}
     >
       {children}
     </button>
