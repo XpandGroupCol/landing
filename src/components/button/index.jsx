0c128559cc7c28dfc01972c92bb173503937906d
XpandGroupCol <Developer@xpandgroup.co>
2023-09-16 16:33:06 -0500

    error espacio entre las dos palabras

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,38 +1,36 @@
 const BUTTON_TYPE = {
-  // boton blanco con primer texto negro y segundo rosado
   primary: {
     bg: "bg-[white]",
-    text: ["text-black ", "text-[#E62057] font-ceraBold"],
+    text: ["text-black", "text-[#E62057] font-ceraBold"],
   },
-  //boton fucsia y letra blanca, ultima con bold
   secondary: {
-    bg:"bg-[#E62057]",
-    text: ["font-ceraLight", "font-ceraLight","font-ceraBold"],
+    bg: "bg-[#E62057]",
+    text: ["font-ceraLight", "font-ceraLight", "font-ceraBold"],
   },
-
-    third: {
+  third: {
     bg: "bg-[#5F4092]",
-    text: ["text-black font-ceraLight", "text-[#5F4092] font-ceraBold",],
+    text: ["text-black font-ceraLight", "text-[#5F4092] font-ceraBold"],
   },
   default: {
     bg: "bg-white",
     text: ["text-gray-300 font-ceraLight", "text-gray-300 font-ceraBold"],
   },
 };
-//creo el objeto
 
 const Button = ({ children, onClick, type = "default" }) => {
   const words = children.split(' ');
   const config = BUTTON_TYPE[type] || BUTTON_TYPE.default;
 
   return (
-    <button onClick={onClick} className={`${config.bg} text-[white] font-ceraLigth border-none h-[46px] text-[24px] leading-[42px] w-full rounded-[30px] px-6 flex justify-center items-center whitespace-nowrap`}>
+    <button onClick={onClick} className={`${config.bg} text-[white] font-ceraLight border-none h-[46px] text-[24px] leading-[42px] w-full rounded-[30px] px-6 flex justify-center items-center`}>
       {words.map((word, index) => (
         <span key={index} className={config.text[index]}>
-          {word + (index !== words.length - 1 ? " " : " ")}
+          {word}{index !== words.length - 1 ? '  ' : ''}
         </span>
       ))}
     </button>
   );
+  
+
 };
-export default Button 
+ export default  Button 
