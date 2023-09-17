82e640a83e57f76b5a16fd0ac4a70c39dc486df4
XpandGroupCol <Developer@xpandgroup.co>
2023-09-17 18:58:22 -0500

    cuadros okay

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -24,8 +24,8 @@ const Button = ({ children, onClick, type = "default" }) => {
   return (
     <button onClick={onClick} className={`${config.bg} text-[white] font-ceraLight border-none h-[46px] text-[24px] leading-[42px] w-full rounded-[30px] px-6 flex justify-center items-center`}>
       {words.map((word, index) => (
-        <span key={index} className={config.text[index]}>
-          {word}{index !== words.length - 1 ? '  ' : ''}
+         <span key={index} className={config.text[index]}>
+          {word}{index !== words.length - 1 ? '' : ''}
         </span>
       ))}
     </button>
