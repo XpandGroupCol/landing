df3894ed02b4e1dd591f82fa7f173ab7d1292dc9
XpandGroupCol <Developer@xpandgroup.co>
2023-09-18 16:13:38 -0500

    input 1 okay

--- src/components/email-input/index.jsx
+++ src/components/email-input/index.jsx
@@ -4,18 +4,22 @@ import WelcomeForm from "../form";
 const EmailInput = () => {
   const [email, setEmail] = useState("");
   return (
-    <div>
+    <div className="justify-center flex gap-3">
       <input
         name="email"
         placeholder="Ingresa tu correo electronico"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        className="w-[408px] pl-[23px] text-[16px] rounded-[50px] h-auto] text-white border-[1px] bg-transparent opacity-[50%] leading-[20.11px] font-ceraLight"
       />
+      <div className="flex">
       <WelcomeForm
         text="Iniciar Prueba Gratuita"
         type="secondary"
         email={email}
+       
       />
+       </div>
     </div>
   );
 };
