80e6c04fa59ee62ba7d953e10ec46e101f03dfcf
XpandGroupCol <Developer@xpandgroup.co>
2023-09-20 14:54:43 -0500

    intentando organizar el input del final parte 1

--- src/components/email-input/index.jsx
+++ src/components/email-input/index.jsx
@@ -4,13 +4,13 @@ import WelcomeForm from "../form";
 const EmailInput = ({type ="secondary"}) => {
   const [email, setEmail] = useState("");
   return (
-    <div className="justify-center flex gap-3 lg:flex-row xxs:flex-col xs:flex-col md:flex-col  ">
+    <div className="justify-center flex gap-3 lg:flex-row xxs:flex-col xs:flex-col md:flex-col  w-auto">
       <input
         name="email"
         placeholder="Ingresa tu correo electronico"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        className="w-[408px] pl-[23px] text-[16px] rounded-[50px] h-auto] text-white border-[1px] bg-transparent opacity-[50%] leading-[20.11px] font-ceraLight"
+        className="w-[408px]  pl-[23px] text-[16px] rounded-[50px] h-auto] text-white border-[1px] bg-transparent opacity-[50%] leading-[20.11px] font-ceraLight"
       />
       <div className="flex">
       <WelcomeForm
