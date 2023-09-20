b7d2125b0f4c21d4c8fa0245cef6d53a97e41e52
XpandGroupCol <Developer@xpandgroup.co>
2023-09-20 18:09:30 -0500

    button hamburguerahecho

--- src/components/hamburguer-menu/index.jsx
+++ src/components/hamburguer-menu/index.jsx
@@ -20,11 +20,11 @@ const HamburguerMenu = () => {
           width="24"
           height="24"
           viewBox="0 0 24 24"
-          stroke-width="2"
+        
           stroke="currentColor"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
           <path d="M4 6l16 0"></path>
@@ -34,22 +34,22 @@ const HamburguerMenu = () => {
         </button>
         <div className={`${menuOpen ? "flex " : "hidden"} top-0 left-0 fixed w-screen h-screen bg-red-500 z-50 flex-col `}>
            <button onClick={toggleMenu} className="absolute top-8 right-8">
-           <svg   width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+           <svg   width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinejoin="round" >
    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
    <path d="M18 6l-12 12"></path>
    <path d="M6 6l12 12"></path>
 </svg>
            </button>
            <div>
-           <div class=" items-center gap-16 flex">
+           <div className=" items-center gap-16 flex">
             <a href="#create-sherflow">
-              <h3 class="font-ceraLight leading-[22.63px] text-[18px]">
-                Paso a paso <span class="font-ceraBold">Campaña</span>
+              <h3 className="font-ceraLight leading-[22.63px] text-[18px]">
+                Paso a paso <span className="font-ceraBold">Campaña</span>
               </h3>
             </a>
             <a href="#benefits">
-              <h3 class="leading-[22.63px] font-ceraLight text-[18px]">
-                Beneficios <span class="font-ceraBold">Shareflow</span>
+              <h3 className="leading-[22.63px] font-ceraLight text-[18px]">
+                Beneficios <span className="font-ceraBold">Shareflow</span>
               </h3>
             </a>
           </div>
